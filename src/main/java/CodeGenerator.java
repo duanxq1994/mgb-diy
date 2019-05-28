d382cfc220aea7a905905d3d962309fce8e96582
BG343674 <xqduan@best-inc.com>
2019-05-28 19:08:45 +0800

    init

---
+++ src/main/java/CodeGenerator.java
@@ -0,0 +1,40 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.mybatis.generator.internal.NullProgressCallback;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * 代码生成器，根据数据表名称生成对应的Model、Mapper、Service、Controller简化开发。
+ */
+public class CodeGenerator {
+
+    private static Configuration config;
+
+    static {
+        try {
+            config = new ConfigurationParser(new ArrayList<String>()).parseConfiguration(CodeGenerator.class.getResourceAsStream("/generatorConfig.xml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //true:覆盖生成
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        try {
+            ArrayList<String> warning = new ArrayList<>();
+            new MyBatisGenerator(config, callback, warning).generate(new NullProgressCallback());
+            if (!warning.isEmpty()) {
+                System.out.println(warning.toString());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("生成失败", e);
+        }
+    }
+
+}
---
+++ src/main/resources/template/mapper.ftl
@@ -0,0 +1,12 @@
+package ${targetPackage};
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author duanxq
+ * Created by duanxq on ${.now?string('yyyy-MM-hh')}.
+ */
+@Repository
+public interface ${tableClass.shortClassName}${suffix} {
+
+}
